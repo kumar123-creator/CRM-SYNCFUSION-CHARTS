6b28f5daa88a01cfd5d2eb30d461a1ceec26d4ec
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-10-26 17:24:43 +0530

    Update Card.svelte

--- src/Card.svelte
+++ src/Card.svelte
@@ -3,21 +3,20 @@
     export let title;
     export let value;
     export let icon;
-    
   </script>
   
   <div class="card">
     {#if icon}
       <div class="icon">
         <i class="fas {icon}"></i> <!-- Use Font Awesome icon class -->
+        <div class="value">{value}</div>
       </div>
     {/if}
-    <div class="title">{title}</div>
-    <div class="value">{value}</div>
+    <div class="title text-muted">{title}</div>
   </div>
   
   <style>
-       .card {
+    .card {
       border: 1px solid #ccc;
       padding: 10px;
       text-align: center;
@@ -27,11 +26,31 @@
     }
   
     .icon {
-      font-size: 24px;
+      display: flex;
+      flex-direction: row; /* Display icon and value side by side */
+      align-items: center; /* Center icon and value vertically */
+    }
+  
+    .icon i {
+      font-size: 15px;
+      margin-right: 0px; /* Add spacing between icon and value */
+    }
+  
+    .value {
+      font-size: 18px; /* You can adjust the font size as needed */
     }
   
     .title {
-      font-weight: bold;
-      margin-top: 10px;
+      font-weight: bolder;
+      
+    }
+  
+    .text-muted {
+      font-weight: 600;
+      font-style: normal;
+      line-height: 20px;
+      font-size: 12px;
+      color: #6c798f !important;
     }
   </style>
+  
